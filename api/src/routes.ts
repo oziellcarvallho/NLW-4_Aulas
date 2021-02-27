48e098160b4bc7290f4ead47fb7a1bb9b94739f5
Oziel Carvalho <oziellcarvallho@gmail.com>
2021-02-27 00:50:10 -0300

    add Aula 05 💻

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveysController } from './controllers/SurveysController';
 import { SendMailController } from './controllers/SendMailController';
+import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.get("/users", userController.show);
 router.post("/users", userController.create);
@@ -15,7 +19,11 @@ router.post("/users", userController.create);
 router.get("/surveys", surveysController.show);
 router.post("/surveys", surveysController.create);
 
-router.get("/sendMail", sendMailController.show)
-router.post("/sendMail", sendMailController.execute)
+router.get("/sendMail", sendMailController.show);
+router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:value", npsController.execute);
 
 export { router }
